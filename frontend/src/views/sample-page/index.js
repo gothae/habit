31b8f7d654307fc72285e29a479a239431b514db
gothae <koxogml@naver.com>
2021-10-25 19:37:54 +0900

    1025

--- frontend/src/views/sample-page/index.js
+++ frontend/src/views/sample-page/index.js
@@ -1,20 +1,27 @@
 // material-ui
-import { Typography } from '@mui/material';
+import { Typography, Grid } from '@mui/material';
 
 // project imports
-import MainCard from 'ui-component/cards/MainCard';
+// import MainCard from 'ui-component/cards/MainCard';
+import DietCard from 'views/dashboard/Default/DietCard';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const SamplePage = () => (
-    <MainCard title="Sample Card">
-        <Typography variant="body2">
-            Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
-            ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-            reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
-            qui officiate descent molls anim id est labours.
-        </Typography>
-    </MainCard>
-);
+const SamplePage = () => {
+    // <MainCard title="Sample Card">
+    //     <Typography variant="body2">
+    //         Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
+    //         ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
+    //         reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
+    //         qui officiate descent molls anim id est labours.
+    //     </Typography>
+    // </MainCard>
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return (
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid></Grid>
+        </Grid>
+    );
+};
 
 export default SamplePage;
