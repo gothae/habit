783302221b1324e883feea2b408e12736822016e
gothae <koxogml@naver.com>
2021-10-25 21:26:04 +0900

    1025

--- frontend/src/views/my-page/index.js
+++ frontend/src/views/my-page/index.js
@@ -1,10 +1,11 @@
 // material-ui
 import { Typography, Grid } from '@mui/material';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 // project imports
 // import MainCard from 'ui-component/cards/MainCard';
 import DietCard from 'views/dashboard/Default/DietCard';
-
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const SamplePage = () => {
@@ -17,6 +18,16 @@ const SamplePage = () => {
     //     </Typography>
     // </MainCard>
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    useEffect(() => {
+        axios
+            .get('http:/localhost:5000/db/test')
+            .then((response) => {
+                console.log('db connected', response);
+            })
+            .catch((error) => {
+                console.log('db fail', error);
+            });
+    });
     // 행렬식으로 card 정렬되게 바꿔야함
     return arr.map((item) => <DietCard key={item} xs={4} />);
 };
