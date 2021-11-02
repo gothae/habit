c7399a5fa061efbe2594f9fb1f225e72173dc72c
gothae <koxogml@naver.com>
2021-11-02 14:10:45 +0900

    날짜 표시

--- frontend/src/views/dashboard/Default/DietCard.js
+++ frontend/src/views/dashboard/Default/DietCard.js
@@ -1,27 +1,12 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
 import React from 'react';
 import SkeletonDietCard from 'ui-component/cards/Skeleton/DietCard';
+import { m } from 'store/constant';
 
 const DietCard = ({ isLoading, value }) => {
-    const m = {
-        Jan: 1,
-        Feb: 2,
-        Mar: 3,
-        Apr: 4,
-        May: 5,
-        Jun: 6,
-        Jul: 7,
-        Aug: 8,
-        Sep: 9,
-        Oct: 10,
-        Nov: 11,
-        Dec: 12
-    };
     const d = value['value'].toString();
     const date = d.slice(8, 10);
     const month = d.slice(4, 7);
-    // console.log(value.getMonth().toString(), typeof value);
-    // console.log(d, typeof d);
     return (
         <div>
             {isLoading ? (
