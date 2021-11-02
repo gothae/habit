5804ba695da0bab9b3beb584f6ab390666245b00
HyunhoSb <hhshin9879@gmail.com>
2021-11-02 14:32:31 +0900

    Update Query_patient

--- backend/models/Query_patient.py
+++ backend/models/Query_patient.py
@@ -23,27 +23,15 @@ class Query_patient(db.model):
     __tablename__ = 'query_patient'
     query_id = db.Column(db.String(20),nullable = False, unique = True, primary_key = True)
     patient_id = db.Column(db.String(20),db.ForeignKey('patient.patient_id'),nullable = False, unique = True)
-    patient_name = db.Column(db.String(20),db,ForeignKey('patient.patient_name'),nullable=False)
-    age = db.Column(db.int,db,ForeignKey('patient.age'),nullable=False)
-    weight = db.Column(db.float,db,ForeignKey('patient.weight'),nullable=False)
-    height = db.Column(db.float,db,ForeignKey('patient.height'),nullable=False)
-    birth_date = db.Column(db.String(8),db,ForeignKey('patient.birth_date'),nullable=False)
-    gender = db.Column(db.String(7),db,ForeignKey('patient.gender'),nullable=False)
     question_01 = db.Column(db.String(100), nullable = False)
     question_02 = db.Column(db.String(100), nullable = False)
     question_03 = db.Column(db.String(100), nullable = False)
     question_04 = db.Column(db.String(100), nullable = False)
     question_05 = db.Column(db.String(100), nullable = False)
 
-    def __init__(self, query_id, patient_id, patient_name, age,weight,height,birth_date, gender, question_01, question_02, question_03, question_04, question_05):
+    def __init__(self, query_id, patient_id, question_01, question_02, question_03, question_04, question_05):
         self.query_id = query_id
         self.patient_id = patient_id
-        self.patient_name = patient_name
-        self.age = age
-        self.weight = weight
-        self.height= height
-        self.birth_date = birth_date
-        self.gender = gender
         self.question_01 = question_01
         self.question_02 = question_02
         self.question_03 = question_03
