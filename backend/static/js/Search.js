952688f8a70c932ceee67b901301441419c04f53
gothae <koxogml@naver.com>
2021-11-10 15:10:59 +0900

    test

--- backend/static/js/Search.js
+++ backend/static/js/Search.js
@@ -27,48 +27,51 @@ function search() {
                 }
             });
         });
-    const b_h = document.createElement('h2');
-    const b_img = document.createElement('img');
-    const b_p = document.createElement('p');
-    const b_button = document.createElement('button');
-    b_h.innerHTML = b[5];
-    b_img.src = b[2];
-    b_img.width = '300px';
-    b_img.height = '300px';
-    b_p.innerHTML = b[4];
-    b_button.addEventListener('click',function(){
-        solution();
-    });
-    breakfast.appendChild(b_h, b_img, b_p, b_button);
-
-    const l_h = document.createElement('h2');
-    const l_img = document.createElement('img');
-    const l_p = document.createElement('p');
-    const l_button = document.createElement('button');
-    l_h.innerHTML = l[5];
-    l_img.src = l[2];
-    l_img.width = '300px';
-    l_img.height = '300px';
-    l_p.innerHTML = l[4];
-    l_button.addEventListener('click', function () {
-        solution();
-    });
-    lunch.appendChild(l_h, l_img, l_p, l_button);
-
-    const d_h = document.createElement('h2');
-    const d_img = document.createElement('img');
-    const d_p = document.createElement('p');
-    const d_button = document.createElement('button');}
-    d_h.innerHTML = d[5];
-    d_img.src = d[2];
-    d_img.width = '300px';
-    d_img.height = '300px';
-    d_p.innerHTML = d[4];
-    d_button.addEventListener('click', function () {
-        solution();
-    });
-    dinner.appendChild(d_h, d_img, d_p, d_button);
-
+    if(length(b) != 0){
+        const b_h = document.createElement('h2');
+        const b_img = document.createElement('img');
+        const b_p = document.createElement('p');
+        const b_button = document.createElement('button');
+        b_h.innerHTML = b[5];
+        b_img.src = b[2];
+        b_img.width = '300px';
+        b_img.height = '300px';
+        b_p.innerHTML = b[4];
+        b_button.addEventListener('click',function(){
+            solution();
+        });
+        breakfast.appendChild(b_h, b_img, b_p, b_button);
+    }
+    if(length(l) != 0){
+        const l_h = document.createElement('h2');
+        const l_img = document.createElement('img');
+        const l_p = document.createElement('p');
+        const l_button = document.createElement('button');
+        l_h.innerHTML = l[5];
+        l_img.src = l[2];
+        l_img.width = '300px';
+        l_img.height = '300px';
+        l_p.innerHTML = l[4];
+        l_button.addEventListener('click', function () {
+            solution();
+        });
+        lunch.appendChild(l_h, l_img, l_p, l_button);
+    }
+    if(length(d) != 0){
+        const d_h = document.createElement('h2');
+        const d_img = document.createElement('img');
+        const d_p = document.createElement('p');
+        const d_button = document.createElement('button');}
+        d_h.innerHTML = d[5];
+        d_img.src = d[2];
+        d_img.width = '300px';
+        d_img.height = '300px';
+        d_p.innerHTML = d[4];
+        d_button.addEventListener('click', function () {
+            solution();
+        });
+        dinner.appendChild(d_h, d_img, d_p, d_button);
+    }
     console.log(b,l,d);
     dietItems.style.display = "block";
 
