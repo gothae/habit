07f139f3a4fc5472caea359dfd70793b5e85b167
gothae <koxogml@naver.com>
2021-11-12 14:43:05 +0900

    Add nutrient.py

---
+++ backend/nutrient.py
@@ -0,0 +1,323 @@
+nutrient = {
+    "밥" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "비빔밥" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "볶음밥" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "김밥" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "국수" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "비빔국수" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "짜장면" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "라면" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "죽" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "맑은국" :
+    {
+    
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "된장국" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "고깃국" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "미역국" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "김칫국" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "생선조림" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "생선구이" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "불고기" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "제육볶음" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "육류구이" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "가공햄" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "계란반찬" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "볶음반찬" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "김치" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "나물반찬" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "떡볶이" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "채소" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "튀김" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "과일" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "디저트빵" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "식사빵" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "소주" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0
+    },
+    "맥주" :
+    {
+        "calories" : 0,
+        "carbohydrate" : 0,
+        "protein" : 0,
+        "fat": 0,
+        "sodium" :0,
+        "calcium" : 0,
+        "vitaminC" : 0 
+    }
+}
