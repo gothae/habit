ba59def9240a03d543ebd933da6e88ee113d2d39
gothae <koxogml@naver.com>
2021-11-18 16:52:13 +0900

    add DaySolutionSearch table보여주기

--- backend/static/js/Search.js
+++ backend/static/js/Search.js
@@ -205,7 +205,7 @@ function DaySolutionSearch(){
     for( var i=0; i<=arr.length; i++){
         var data = `<tr>
                         <td>${arr[i].name}</td>
-                        <td>${arr[i].amount}
+                        <td>${arr[i].amount}</td>
                     </tr>`;
         tableContent.innerHTML += data;
     }
